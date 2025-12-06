df26794e5643888919e53123796ee1a51250260a
Harpreet Singh <happu20022@gmail.com>
2025-12-07 00:06:33 +0530

    made changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  base:'/svelete-gita/',
   server: {
     proxy: {
       "/geeta-api": {
